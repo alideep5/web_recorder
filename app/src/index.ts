147c63f3a907da44dd0c7d86b41e76bdd329af2b
Ali <ali.akbar@serafin-am.com>
2024-10-19 20:15:36 +0200

    feat: add proper example

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,2 +1,17 @@
-const message: string = 'Hello world'
-console.log(message)
+import { chromium, Page } from 'playwright-core';
+
+(async () => {
+    const browser = await chromium.launch({
+        headless: false,
+        executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+    });
+    
+    const page:Page = await browser.newPage();
+
+    page.context().on('onaction', (data:Object) => {
+        console.log('Action', data);
+    });
+
+    await page.goto('https://www.google.com/');
+    await page.pause();
+})();
