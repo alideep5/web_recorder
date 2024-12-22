ba5c57bab48ee2e5a123d5b56e79f384730631bc
Ali <ali.akbar@serafin-am.com>
2024-12-22 16:28:38 +0100

    locator

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,10 +1,4 @@
-import { chromium, Page } from 'playwright-core';
-
-declare global {
-    interface Window {
-        onHoverElement(x: any, y: any): void;
-    }
-}
+import { chromium, Page } from 'playwright-core-a';
 
 (async () => {
     const browser = await chromium.launch({
@@ -12,27 +6,76 @@ declare global {
         executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
     });
 
-    const page: Page = await browser.newPage();
+
+    const context = await browser.newContext({
+        viewport: null,
+    });
+
+    const page: Page = await context.newPage();
 
     await page.goto('https://www.facebook.com/');
 
-    // await new Promise(resolve => setTimeout(resolve, 5000));
-
-    // await page.screenshot({ path: 'img.png', fullPage: false })
-
-    await page.exposeFunction('onHoverElement', (x: any, y: any) => {
-        console.log("********** DOOOM **********");
-        console.log(x);
-        console.log(y);
+    context.on('onaction', async (data: any) => {
+        if (data['action'] === 'inspecting') {
+            console.log(data);
+        }
     });
 
-    await page.evaluate(() => {
-        document.addEventListener("mousemove", function (e) {
-            if (e.ctrlKey === true) {
-                window.onHoverElement(e.clientX, e.clientY);
+    setTimeout(async () => {
+        const selectorElement = await page.evaluateHandle(({ x, y }) => {
+            const getPos = (element: any) => {
+                let position = 1;
+                for (let curNode = element.previousSibling; curNode; curNode = curNode.previousSibling) {
+                    if (curNode.nodeName == element.nodeName) {
+                        ++position;
+                    }
+                }
+                return position;
             }
-        });
-    });
+
+            const getXpath = (htmlElement: any) => {
+                try {
+                    let xpath = '';
+                    for (let element = htmlElement; element && !(element instanceof Document); element = element.nodeType === Node.ATTRIBUTE_NODE ? element.ownerElement : element.parentNode) {
+                        let nodeName;
+                        switch (element.nodeType) {
+                            case Node.TEXT_NODE:
+                                nodeName = 'text()';
+                                break;
+                            case Node.ATTRIBUTE_NODE:
+                                nodeName = '@' + element.nodeName;
+                                break;
+                            case Node.PROCESSING_INSTRUCTION_NODE:
+                                nodeName = 'processing-instruction()';
+                                break;
+                            case Node.COMMENT_NODE:
+                                nodeName = 'comment()';
+                                break;
+                            case Node.ELEMENT_NODE:
+                                nodeName = element.nodeName;
+                                break;
+                        }
+                        const position = getPos(element);
+                        xpath = '/' + nodeName.toLowerCase() + (position !== null ? '[' + position + ']' : '') + xpath;
+                    }
+                    return xpath;
+                } catch { }
+
+                return "";
+            };
+
+            return getXpath(document.elementFromPoint(x, y));
+        }, { x: 755, y: 620 });
+
+        const fullXpath = await selectorElement.evaluate((obj) => obj);
+
+        await page.locator(`xpath=${fullXpath}`).click();
+
+        console.log('clicked');
+    }, 5000);
+
+
+    await page.pause();
 
 })();
 
